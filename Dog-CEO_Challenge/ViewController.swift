51367a52a75132b4ae135591d559394d9db72df2
Fernando <fernando.eb@icloud.com>
2021-12-23 10:15:25 -0300

    Initial Commit

---
+++ Dog-CEO_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dog-CEO_Challenge
+//
+//  Created by MobDev on 23-12-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
